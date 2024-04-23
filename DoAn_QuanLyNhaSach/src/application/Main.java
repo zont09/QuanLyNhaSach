2554e3dd60ad75abfb11e97021b53d5c21a5f507
hungt <hungt@ThinhKe>
2024-04-23 13:29:24 +0700

    UPDATE

--- DoAn_QuanLyNhaSach/src/application/Main.java
+++ DoAn_QuanLyNhaSach/src/application/Main.java
@@ -10,11 +10,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/MainWindow.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/BookInputWindow.fxml"));
 	        Parent root = loader.load();
 	        Scene scene = new Scene(root);
 	        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 	        primaryStage.setScene(scene);
+	        primaryStage.setTitle("Bookstore");
 	        primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
